59aee37677860df004b0eed00eee2d7ef5d0737c
ambitious <1535820771@qq.com>
2023-09-14 14:35:34 +0800

    fixed emby-live.js

--- emby2Alist/nginx/conf.d/emby-live.js
+++ emby2Alist/nginx/conf.d/emby-live.js
@@ -24,31 +24,63 @@ async function directLive(r) {
     redirect2Origin(r, embyHost);
     return;
   }
-  r.warn(`mount emby file path: ${embyRes}`);
-  // 4 execute redirect
-  if (!checkM3U8(embyRes)) {
-    redirect2Origin(r, embyHost);
-    return;
+  if (!checkLive(embyRes)) {
+    const host = r.headersIn['Host'];
+    const uri = r.uri.replace("master.m3u8", "stream.mp4");
+    const url = generateUrl(r, host, uri);
+    r.warn(`stream url: ${url}`);
+    const panUrl = await attemptPanUrl(url);
+    if (panUrl) {
+      r.warn(`pan url: ${panUrl}`);
+      return r.return(302, panUrl);
+    }
+    return r.return(302, url);
   }
+  r.warn(`mount emby file path: ${embyRes}`);
+  // 5 execute redirect
   r.return(302, embyRes);
 }
 
-function checkM3U8(url) {
+// 尝试请求 stream 流，看请求结果是否是网盘直链重定向地址
+async function attemptPanUrl(streamUrl) {
+  const response = await ngx.fetch(streamUrl, {
+    method: "GET"
+  });
+  if (response && response.headers["Location"] && response.status === 302) {
+    return response.headers["Location"];
+  }
+  return null;
+}
+
+// 检查获取的链接是否是直播源，而非本地的资源路径
+function checkLive(url) {
   if (!url || url === "") {
     return false;
   }
-  return url.includes("m3u");
+  const regex = /^([^:/?#]+):/;
+  const match = url.match(regex);
+  if (!match) {
+    return false;
+  }
+  const protocol = match[1].toLowerCase();
+  const valids = ["http", "https", "hls", "rtsp", "rtmp"];
+  return valids.findIndex((v) => v === protocol) !== -1;
 }
 
 function redirect2Origin(r, embyHost) {
-  let url = embyHost + r.uri;
+  const url = generateUrl(r, embyHost, r.uri);
+  r.return(302, url);
+}
+
+function generateUrl(r, host, uri) {
+  let url = "http://" + host + uri;
   let isFirst = true;
   for (const key in r.args) {
     url += isFirst ? "?" : "&";
     url += `${key}=${r.args[key]}`;
     isFirst = false;
   }
-  r.return(302, url);
+  return url;
 }
 
 export default { directLive };
