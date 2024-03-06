74c7a7fe461c8364b2c73dec426727acb41bab96
chen3861229 <chen3861229@hotmail.com>
2024-03-06 22:00:23 +0800

    fix(emby2Alist): 修复filePath为空情况

--- emby2Alist/nginx/conf.d/util.js
+++ emby2Alist/nginx/conf.d/util.js
@@ -69,6 +69,9 @@ function strMatches(type, searchValue, matcher) {
 }
 
 function checkIsRemoteStrm(protocol, filePath) {
+  if (!protocol || !filePath) {
+    return false;
+  }
   return "File" != protocol && filePath.toLowerCase().endsWith(".strm");
 }
 
