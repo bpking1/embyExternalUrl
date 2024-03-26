646f7eb2336971ce396e4d31a186e9cd024c2569
chen3861229 <chen3861229@hotmail.com>
2024-03-26 22:21:44 +0800

    feat(plex2Alist): 修复了plex对strm文件下载的重定向

--- plex2Alist/nginx/conf.d/util.js
+++ plex2Alist/nginx/conf.d/util.js
@@ -1,12 +1,15 @@
 import config from "./constant.js";
 
-const plexTokenKey = "X-Plex-Token";
+const args = {
+  plexTokenKey: "X-Plex-Token",
+}
+
 // copy from emby2Alist/nginx/conf.d/util.js
 function proxyUri(uri) {
   return `/proxy${uri}`;
 }
 
-function isDisableRedirect(r, str, isAlistRes) {
+function isDisableRedirect(str, isAlistRes) {
   let arr2D;
   if (!!isAlistRes) {
     // this var isAlistRes = true
@@ -37,6 +40,14 @@ function strMatches(type, searchValue, matcher) {
   return false;
 }
 
+function checkIsStrmByPath(filePath) {
+  if (!!filePath) {
+    // strm: filePath1-itemPath like: /xxx/xxx.strm
+    return filePath.toLowerCase().endsWith(".strm");
+  }
+  return false;
+}
+
 // plex only
 function getFileNameByHead(contentDisposition) {
   if (contentDisposition && contentDisposition.length > 0) {
@@ -46,53 +57,11 @@ function getFileNameByHead(contentDisposition) {
   return null;
 }
 
-async function getPlexItemInfo(r) {
-  const plexHost = config.plexHost;
-  const path = r.args.path;
-  const mediaIndex = r.args.mediaIndex;
-  const partIndex = r.args.partIndex;
-  const api_key = r.args[plexTokenKey];
-  let filePath;
-  let itemInfoUri = "";
-  if (path) {
-	  // see: location ~* /video/:/transcode/universal/start
-  	itemInfoUri = `${plexHost}${path}?${plexTokenKey}=${api_key}`;
-  } else {
-  	// see: location ~* /library/parts/(\d+)/(\d+)/file
-    filePath = ngx.shared.partInfoDict.get(r.uri);
-    r.warn(`getPlexItemInfo r.uri: ${r.uri}`);
-    if (!filePath) {
-      const plexRes = await fetchPlexFileFullName(`${plexHost}${r.uri}?download=1&${plexTokenKey}=${api_key}`);
-      if (!plexRes.startsWith("error")) {
-        const plexFileName = plexRes.substring(0, plexRes.lastIndexOf("."));
-        itemInfoUri = `${plexHost}/search?query=${encodeURI(plexFileName)}&${plexTokenKey}=${api_key}`;
-      } else {
-        r.warn(plexRes);
-      }
-    }
-  }
-  return { filePath, itemInfoUri, mediaIndex, partIndex, api_key };
-}
-
-async function fetchPlexFileFullName(downloadApiPath) {
-  try {
-    const response = await ngx.fetch(downloadApiPath, {
-      method: "HEAD",
-      max_response_body_size: 858993459200 // 100Gb,not important,because HEAD method not have body
-    });
-    if (response.ok) {
-      return getFileNameByHead(decodeURI(response.headers["Content-Disposition"]));
-    } else {
-      return `error: plex_download_api ${response.status} ${response.statusText}`;
-    }
-  } catch (error) {
-    return `error: plex_download_api fetchPlexFileNameFiled ${error}`;
-  }
-}
-
 export default {
+  args,
   proxyUri,
   isDisableRedirect,
+  checkIsStrmByPath,
   strMatches,
-  getPlexItemInfo,
+  getFileNameByHead,
 };
