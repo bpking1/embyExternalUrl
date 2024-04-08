884c00545d7e9e338f5e6dd8d2fc05c0835dab64
chen3861229 <chen3861229@hotmail.com>
2024-04-08 21:54:35 +0800

    refactor(emby2Alist&plex2Alist): 按照代码处理顺序同步配置项中先后

--- emby2Alist/nginx/conf.d/exampleConfig/constant-strm.js
+++ emby2Alist/nginx/conf.d/exampleConfig/constant-strm.js
@@ -8,34 +8,8 @@ const embyHost = "http://172.17.0.1:8096";
 const embyApiKey = "f839390f50a648fd92108bc11ca6730a";
 
 // 选填项,用不到保持默认即可
-// 路径映射,会在xxxMountPath之后从上到下依次全部替换一遍,不要有重叠
-// 参数1: 0: 默认做字符串替换, 1: 前插, 2: 尾插
-// 参数2: 0: 默认只处理/开头的路径且不为strm, 1: 只处理strm内部为/开头的相对路径, 2: 只处理strm内部为远程链接的
-// 参数3: 来源, 参数4: 目标
-const embyPathMapping = [
-  // [0, 0, "/mnt/aliyun-01", "/mnt/aliyun-02"],
-  // [0, 2, "http:", "https:"], 
-  // [0, 2, ":5244", "/alist"], 
-  // [0, 0, "D:", "F:"],
-  // [0, 0, /blue/g, "red"], // 此处正则不要加引号
-  // [1, 1, `${alistPublicAddr}/d`],
-  // [2, 2, "?xxx"],
-];
 // 局域网ip头
 const lanIpHead = ["172.", "10.", "192.", "[fd00:"];
-// 指定是否转发由njs获取strm重定向后直链地址的规则,例如strm内部为局域网ip或链接或需要请求头验证
-// 参数1: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
-// 参数2: 匹配目标,对象为xxxPathMapping映射后的strm内部链接
-// 参数3: 请求验证类型,已为直链的不需要此参数,例如.../d
-const redirectStrmLastLinkRule = [
-  [0, lanIpHead.map(s => "http://" + s)], 
-  // [0, alistAddr], 
-  // [0, "http:"], 
-  // // 参数4: 已为直链的不需要此参数, 参数暂无作用, sign属于额外验证
-  // [0, "http://otheralist1.com", "FixedToken", alistToken], 
-  // // arg4: 已为直链的不需要此参数,额外指定调用登录接口的api地址, 参数暂无作用, sign属于额外验证
-  // [0, "http://otheralist2.com", "TempToken", `read:123456`, `http://otheralist2.com:5244/api/auth/login`], 
-];
 // 禁用直链的规则,将转给原始媒体服务器处理,字幕和图片没有走直链,不用添加
 // 参数1: 匹配类型或来源(字符串参数类型) "0": 文件路径(Item.Path)
 // 参数2: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
@@ -54,6 +28,32 @@ const disableRedirectRule = [
   // ["r.args.X-Emby-Device-Name", 0, "Microsoft Edge Windows"], // 链接入参,设备名称
   // ["r.args.X-Emby-Client", 0, "Emby Web"], // 链接入参,客户端类型
   // ["r.args.UserId", 0, "ac0d220d548f43bbb73cf9b44b2ddf0e"], // 链接入参,用户id
+];
+// 路径映射,会在xxxMountPath之后从上到下依次全部替换一遍,不要有重叠
+// 参数1: 0: 默认做字符串替换, 1: 前插, 2: 尾插
+// 参数2: 0: 默认只处理/开头的路径且不为strm, 1: 只处理strm内部为/开头的相对路径, 2: 只处理strm内部为远程链接的
+// 参数3: 来源, 参数4: 目标
+const embyPathMapping = [
+  // [0, 0, "/mnt/aliyun-01", "/mnt/aliyun-02"],
+  // [0, 2, "http:", "https:"], 
+  // [0, 2, ":5244", "/alist"], 
+  // [0, 0, "D:", "F:"],
+  // [0, 0, /blue/g, "red"], // 此处正则不要加引号
+  // [1, 1, `${alistPublicAddr}/d`],
+  // [2, 2, "?xxx"],
+];
+// 指定是否转发由njs获取strm重定向后直链地址的规则,例如strm内部为局域网ip或链接或需要请求头验证
+// 参数1: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
+// 参数2: 匹配目标,对象为xxxPathMapping映射后的strm内部链接
+// 参数3: 请求验证类型,已为直链的不需要此参数,例如.../d
+const redirectStrmLastLinkRule = [
+  [0, lanIpHead.map(s => "http://" + s)], 
+  // [0, alistAddr], 
+  // [0, "http:"], 
+  // // 参数4: 已为直链的不需要此参数, 参数暂无作用, sign属于额外验证
+  // [0, "http://otheralist1.com", "FixedToken", alistToken], 
+  // // arg4: 已为直链的不需要此参数,额外指定调用登录接口的api地址, 参数暂无作用, sign属于额外验证
+  // [0, "http://otheralist2.com", "TempToken", `read:123456`, `http://otheralist2.com:5244/api/auth/login`], 
 ];
 // 图片缓存策略,包括主页、详情页、图片库的原图,路由器nginx请手动调小conf中proxy_cache_path的max_size
 // 0: 不同尺寸设备共用一份缓存,先访问先缓存,空间占用最小但存在小屏先缓存大屏看的图片模糊问题
@@ -97,7 +97,8 @@ const alistAddr = "";
 const alistToken = "";
 // alist公网地址, 用于需要alist server代理流量的情况, 按需填写
 const alistPublicAddr = "";
-// 指定客户端自己请求并获取alist直链的规则,特殊情况使用,则此处必须使用域名且公网畅通,用不着请保持默认
+// 指定客户端自己请求并获取alist直链的规则,代码优先级在redirectStrmLastLinkRule之后
+// 特殊情况使用,则此处必须使用域名且公网畅通,用不着请保持默认
 // 参数1: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
 // 参数2: 匹配目标,对象为Alist接口返回的链接raw_url
 // 参数3: 指定转发给客户端的alist的host前缀
@@ -137,5 +138,5 @@ export default {
   transcodeBalanceConfig,
   getEmbyHost,
   getEnableTranscodeBalance,
-  getImageCachePolicy
+  getImageCachePolicy,
 }
--- plex2Alist/nginx/conf.d/exampleConfig/constant-strm.js
+++ plex2Alist/nginx/conf.d/exampleConfig/constant-strm.js
@@ -6,41 +6,8 @@
 const plexHost = "http://172.17.0.1:32400";
 
 // 选填项,用不到保持默认即可
-// 指定客户端自己请求并获取alist直链的规则,特殊情况使用,则此处必须使用域名且公网畅通,用不着请保持默认
-// 参数1: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
-// 参数2: 匹配目标,对象为Alist接口返回的链接raw_url
-// 参数3: 指定转发给客户端的alist的host前缀
-const cilentSelfAlistRule = [
-  // [2, "xxx", alistPublicAddr],
-];
-// 路径映射,会在xxxMountPath之后从上到下依次全部替换一遍,不要有重叠
-// 参数1: 0: 默认做字符串替换, 1: 前插, 2: 尾插
-// 参数2: 0: 默认只处理/开头的路径且不为strm, 1: 只处理strm内部为/开头的相对路径, 2: 只处理strm内部为远程链接的
-// 参数3: 来源, 参数4: 目标
-const plexPathMapping = [
-  // [0, 0, "/mnt/aliyun-01", "/mnt/aliyun-02"],
-  // [0, 2, "http:", "https:"], 
-  // [0, 2, ":5244", "/alist"], 
-  // [0, 0, "D:", "F:"],
-  // [0, 0, /blue/g, "red"], // 此处正则不要加引号
-  // [1, 1, `${alistPublicAddr}/d`],
-  // [2, 2, "?xxx"],
-];
 // 局域网ip头
 const lanIpHead = ["172.", "10.", "192.", "[fd00:"];
-// 指定是否转发由njs获取strm重定向后直链地址的规则,例如strm内部为局域网ip或链接或需要请求头验证
-// 参数1: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
-// 参数2: 匹配目标,对象为xxxPathMapping映射后的strm内部链接
-// 参数3: 请求验证类型,已为直链的不需要此参数,例如.../d
-const redirectStrmLastLinkRule = [
-  [0, lanIpHead.map(s => "http://" + s)], 
-  // [0, alistAddr], 
-  // [0, "http:"], 
-  // // 参数4: 已为直链的不需要此参数, 参数暂无作用, sign属于额外验证
-  // [0, "http://otheralist1.com", "FixedToken", alistToken], 
-  // // arg4: 已为直链的不需要此参数,额外指定调用登录接口的api地址, 参数暂无作用, sign属于额外验证
-  // [0, "http://otheralist2.com", "TempToken", `read:123456`, `http://otheralist2.com:5244/api/auth/login`], 
-];
 // 禁用直链的规则,将转给原始媒体服务器处理,字幕和图片没有走直链,不用添加
 // 参数1: 匹配类型或来源(字符串参数类型) "0": 文件路径(Item.Path)
 // 参数2: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
@@ -59,6 +26,40 @@ const disableRedirectRule = [
   // ["r.args.X-Emby-Device-Name", 0, "Microsoft Edge Windows"], // 链接入参,设备名称
   // ["r.args.X-Emby-Client", 0, "Emby Web"], // 链接入参,客户端类型
   // ["r.args.UserId", 0, "ac0d220d548f43bbb73cf9b44b2ddf0e"], // 链接入参,用户id
+];
+// 路径映射,会在xxxMountPath之后从上到下依次全部替换一遍,不要有重叠
+// 参数1: 0: 默认做字符串替换, 1: 前插, 2: 尾插
+// 参数2: 0: 默认只处理/开头的路径且不为strm, 1: 只处理strm内部为/开头的相对路径, 2: 只处理strm内部为远程链接的
+// 参数3: 来源, 参数4: 目标
+const plexPathMapping = [
+  // [0, 0, "/mnt/aliyun-01", "/mnt/aliyun-02"],
+  // [0, 2, "http:", "https:"], 
+  // [0, 2, ":5244", "/alist"], 
+  // [0, 0, "D:", "F:"],
+  // [0, 0, /blue/g, "red"], // 此处正则不要加引号
+  // [1, 1, `${alistPublicAddr}/d`],
+  // [2, 2, "?xxx"],
+];
+// 指定是否转发由njs获取strm重定向后直链地址的规则,例如strm内部为局域网ip或链接或需要请求头验证
+// 参数1: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
+// 参数2: 匹配目标,对象为xxxPathMapping映射后的strm内部链接
+// 参数3: 请求验证类型,已为直链的不需要此参数,例如.../d
+const redirectStrmLastLinkRule = [
+  [0, lanIpHead.map(s => "http://" + s)], 
+  // [0, alistAddr], 
+  // [0, "http:"], 
+  // // 参数4: 已为直链的不需要此参数, 参数暂无作用, sign属于额外验证
+  // [0, "http://otheralist1.com", "FixedToken", alistToken], 
+  // // arg4: 已为直链的不需要此参数,额外指定调用登录接口的api地址, 参数暂无作用, sign属于额外验证
+  // [0, "http://otheralist2.com", "TempToken", `read:123456`, `http://otheralist2.com:5244/api/auth/login`], 
+];
+// 指定客户端自己请求并获取alist直链的规则,代码优先级在redirectStrmLastLinkRule之后
+// 特殊情况使用,则此处必须使用域名且公网畅通,用不着请保持默认
+// 参数1: 0: startsWith(str), 1: endsWith(str), 2: includes(str), 3: match(/ain/g)
+// 参数2: 匹配目标,对象为Alist接口返回的链接raw_url
+// 参数3: 指定转发给客户端的alist的host前缀
+const cilentSelfAlistRule = [
+  // [2, "xxx", alistPublicAddr],
 ];
 
 // 留空项,不要更改
@@ -86,5 +87,5 @@ export default {
   cilentSelfAlistRule,
   plexPathMapping,
   redirectStrmLastLinkRule,
-  getPlexHost
+  getPlexHost,
 }
