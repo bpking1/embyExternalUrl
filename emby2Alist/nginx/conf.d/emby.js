24a72e7c70b4471800621da1988b2872ff373b5e
chen3861229 <chen3861229@hotmail.com>
2024-06-29 20:50:30 +0800

    fix(emby2Alist): 修复转码切换逻辑

--- emby2Alist/nginx/conf.d/emby.js
+++ emby2Alist/nginx/conf.d/emby.js
@@ -250,16 +250,13 @@ async function transferPlaybackInfo(r) {
           source.XRouteMode = routeMode; // for debug
           if (util.ROUTE_ENUM.redirect == routeMode) {
             if (!transcodeConfig.redirectTransOptEnable) source.SupportsTranscoding = false;
-            // first priority is user clients choice video bitrate < source.Bitrate
-            const maxStreamingBitrate = parseInt(r.args.MaxStreamingBitrate);
-            if (r.args.StartTimeTicks !== "0" && maxStreamingBitrate < source.Bitrate) {
-              r.warn(`playback started swich redirect to transcode by opt`);
-              modifyDirecPlaySupports(source, false);
-            }
-            // cover routeMode
-            // source.TranscodingUrl is important, sometimes SupportsTranscoding true but it's empty
-            if (!isStrm && r.args.StartTimeTicks === "0" && source.SupportsTranscoding && source.TranscodingUrl) {
-              // if (source.AutoOpenLiveStream === "true" && source.SupportsTranscoding) {
+            // 1. first priority is user clients choice video bitrate < source.Bitrate
+            // 2. strict cover routeMode, do't use r.args.StartTimeTicks === "0"
+            // 3. source.TranscodingUrl is important, sometimes SupportsTranscoding true but it's empty
+            // 4. ensure transcoding experience, disable STRM file transcode, because clients 1 ts part only wait 10s forced disconnection
+            const maxStreamingBitrate = parseInt(r.args.MaxStreamingBitrate); 
+            if (!isStrm && maxStreamingBitrate < source.Bitrate 
+              && source.SupportsTranscoding && source.TranscodingUrl) {
               r.warn(`client reported and server judgment to transcode, cover routeMode`);
               source.XRouteMode = util.ROUTE_ENUM.transcode; // for debug
               modifyDirecPlaySupports(source, false);
