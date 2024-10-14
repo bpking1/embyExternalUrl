2605105a16ca3e7200d5db061bdbd8fed0983f5f
chen3861229 <chen3861229@hotmail.com>
2024-10-15 00:34:26 +0800

    fix(emby2Alist&plex2Alist): 精确远程链接的解码范围

--- emby2Alist/nginx/conf.d/emby.js
+++ emby2Alist/nginx/conf.d/emby.js
@@ -63,8 +63,11 @@ async function redirect2Pan(r) {
   // strm file internal text maybe encode
   r.warn(`notLocal: ${embyRes.notLocal}`);
   if (embyRes.notLocal) {
-    embyRes.path = decodeURIComponent(embyRes.path);
-    r.warn(`notLocal decodeURIComponent embyRes.path`);
+    const filePathPart = urlUtil.getFilePathPart(embyRes.path);
+    if (filePathPart) {
+      r.warn(`notLocal is CloudDrive/AList link, decodeURIComponent embyRes.path before: ${embyRes.path}`);
+      embyRes.path = decodeURIComponent(embyRes.path);
+    }
   }
 
   // check symlinkRule
